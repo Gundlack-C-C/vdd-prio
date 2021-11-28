aff7835a7c5d20872d925d43d3b51624216ef4c3
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-11-28 18:46:05 +0100

    remove form and move to databinding behavior

--- vdd-prio/src/app/prio/prio-const-sum/prio-const-sum.component.ts
+++ vdd-prio/src/app/prio/prio-const-sum/prio-const-sum.component.ts
@@ -11,30 +11,30 @@ export class PrioConstSumComponent implements OnInit {
   @Input() maxSum: number = 100
   @Input() items: {label: string, description: string, value: number}[] = []
   @Output() onPrioChanged = new EventEmitter<number[]>();
+  @Output() onPrioSubmit = new EventEmitter<number[]>();
 
-
-  prioForm = this.formBuilder.group({});
-
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   get SUM(): number {
     const sum = (previousValue: number, currentValue: number) => previousValue + currentValue;
     return this.items.map((item) => item.value).reduce(sum)
   }
 
-  ngOnInit() {
-    let group: {[key: string]: number} =  {};
-    this.items.forEach((item,i: number) => {
-      group['prio-' + i] = 0
+  get PRIO(): number[] {
+    return this.items.map((item) => {
+      return item.value;
     });
-    this.prioForm = this.formBuilder.group(group);
+  }
+
+  ngOnInit() {
+  }
+
+  onValueChanged(index: number, item: any) {
+    this.onPrioChanged.next(this.PRIO)
   }
 
   onSubmit() {
-    let prio = this.prioForm.value;
-    this.onPrioChanged.next(prio)
+    this.onPrioSubmit.next()
   }
 
 }
