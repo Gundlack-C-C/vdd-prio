078d115cd649ed471b41e32602b96dfb465d30a6
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-12-01 18:43:07 +0100

    remove unused routes

--- vdd-prio/src/app/app-routing.module.ts
+++ vdd-prio/src/app/app-routing.module.ts
@@ -6,11 +6,9 @@ import { AuthViewComponent } from './auth/AuthView/AuthView.component';
 import { BusinessAdminComponent } from './business-admin/business-admin.component';
 import { BusinessAppComponent } from './flowcollect/business-app/business-app.component';
 import { BusinessLandingpageComponent } from './flowcollect/business-landingpage/business-landingpage.component';
-import { FlowcollectComponent } from './flowcollect/flowcollect.component';
 import { MentalDashboardComponent } from './flowcollect/mental-dashboard/mental-dashboard.component';
 import { MentalFeedbackAppComponent } from './flowcollect/mental-feedback-app/mental-feedback-app.component';
 import { GraphicsDemoComponent } from './graphics/graphics-demo/graphics-demo.component';
-import { HomeComponent } from './Home/Home.component';
 import { PageNotFoundComponent } from './PageNotFound/PageNotFound.component';
 import { PollDetailViewComponent } from './poll/poll-detail-view/poll-detail-view.component';
 import { PollSectionComponent } from './poll/poll-section/poll-section.component';
@@ -27,11 +25,6 @@ const routes: Routes = [
   { path: 'flowcollect-app/:sectionID', component: MentalFeedbackAppComponent},
   {
     path: 'flowcollect-app-unternehmer',
-    component: BusinessAppComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'flowcollect-app-unternehmer/:businessID',
     component: BusinessAppComponent,
     canActivate: [AuthGuard]
   },
