48e904ba76c0ebe9c701f1e673a9c8098b92a6e8
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-12-03 13:15:55 +0100

    add logic of poll data transition into feedback service

--- vdd-prio/src/app/flowcollect/feedback.service.ts
+++ vdd-prio/src/app/flowcollect/feedback.service.ts
@@ -2,6 +2,57 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/firestore';
 import firebase from "firebase/app";
 import { Observable } from 'rxjs';
+import * as d3 from 'd3';
+
+export function getDateOnly(T: Date) {
+  T.setMilliseconds(0);
+  T.setSeconds(0);
+  T.setMinutes(0);
+  T.setHours(0)
+  return T;
+}
+
+
+export function getDictionaryArray(items: any[]) {
+  let data: {key: string, val: number, T: string, section: string}[] = []
+  items.forEach((item: {createdAt: any, values: {[key: string]: number[]}}) => {
+    let T = getDateOnly(new Date(item.createdAt.toMillis())).toDateString();
+
+    Object.values(item.values).forEach((values, section) => {
+        values.forEach((val, i)=>{
+          data.push({key: i.toString(),val: val, T: T, section: section.toString() })
+        });
+    });
+  });
+  return data;
+}
+
+export function getSectionStatistics(items: {key: string, val: number, T: string, section: string}[], d3_format=false) {
+  let section_group = d3.group(items, d => d.section)
+  let statistics: {[section: string]: any[]} = {};
+  section_group.forEach((item, section)=>{
+    let stats: any[][] = [];
+
+    if(d3_format)
+      stats.push(['key'].concat(...d3.group(item, d=> d.T).keys()))
+
+    d3.group(item, d => d.key, d=>d.T).forEach((item_key, key) => {
+      let key_stats = [];
+
+      if(d3_format)
+        key_stats.push(key);
+
+      item_key.forEach((item_T, T) => {
+        let med = d3.median(item_T, d=>d.val)
+        key_stats.push(med ? med.toString() : "0")
+      })
+      stats.push(key_stats)
+    });
+
+    statistics[section] = stats;
+  });
+  return statistics;
+}
 
 export class POLL {
   values: number[][] = [];
