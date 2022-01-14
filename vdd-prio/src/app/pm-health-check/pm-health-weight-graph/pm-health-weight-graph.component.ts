9b3adf590a9516f3a14e3e8c8e61a879ed507c2c
Felix Gundlack <gundlackfelixdev@gmail.com>
2022-01-14 11:23:42 +0100

    simplify landing page

--- vdd-prio/src/app/pm-health-check/pm-health-weight-graph/pm-health-weight-graph.component.ts
+++ vdd-prio/src/app/pm-health-check/pm-health-weight-graph/pm-health-weight-graph.component.ts
@@ -1,70 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HEALTH_WEIGHT} from '../pm-health-model';
 
-function options_barchart_polar(dataset: any[]) {
-  return {
-    tooltip: {},
-    dataset: {
-      source: dataset
-    },
-    backgroundColor: '#2c343c',
-    title: [
-      {
-        text: 'Erfolg',
-        left: '25%',
-        top: '45%',
-        textAlign: 'center',
-        textStyle: {
-          color: '#198754',
-          fontSize: 25,
-          textShadowBlur: 0.5
-        }
-      },
-      {
-        text: 'Misserfolg',
-        left: '75%',
-        top: '45%',
-        textAlign: 'center',
-        textStyle: {
-          color: '#ffc107',
-          fontSize: 25
-        }
-      },
-    ],
-    visualMap: {
-      show: false,
-      min: 5,
-      max: 13,
-      inRange: {
-        colorLightness: [0.8, 0.2]
-      }
-    },
-    series: [
-      {
-        type: 'pie',
-        name: 'Erfolg',
-        radius: ['40%', '50%'],
-        center: ['25%', '50%'],
-        color: '#198754',
-        encode: {
-          itemName: 'Key',
-          value: 'Erfolg'
-        }
-      },
-      {
-        type: 'pie',
-        name: 'Misserfolg',
-        radius: ['40%', '50%'],
-        center: ['75%', '50%'],
-        color: '#ffc107',
-        encode: {
-          itemName: 'Key',
-          value: 'Misserfolg'
-        }
-      }
-    ]
-  };
-}
+
 function options_barchart_stacked(keys: string[], data: {erfolg: number[], misserfolg: number[]}) {
   const series = keys.map((item, i) => {
     return {
@@ -124,52 +61,6 @@ function options_barchart_stacked(keys: string[], data: {erfolg: number[], misse
   };
 }
 
-function options_barchart(keys: string[], data: {erfolg: number[], misserfolg: number[]}) {
-   return {
-    title: {
-      text: 'GPM - PM Studie 2008'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow'
-      }
-    },
-    legend: {},
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis: {
-      type: 'value',
-      boundaryGap: [0, 0.01],
-      axisLabel: {
-        formatter: "{value} %"
-      }
-    },
-    yAxis: {
-      type: 'category',
-      data: keys
-    },
-    series: [
-      {
-        name: 'Erfolg',
-        type: 'bar',
-        data: data.erfolg,
-        color: '#198754'
-      },
-      {
-        name: 'Misserfolg',
-        type: 'bar',
-        data: data.misserfolg,
-        color: '#ffc107'
-      }
-    ]
-  };
-}
-
 @Component({
   selector: 'app-pm-health-weight-graph',
   templateUrl: './pm-health-weight-graph.component.html',
@@ -177,29 +68,12 @@ function options_barchart(keys: string[], data: {erfolg: number[], misserfolg: n
 })
 export class PmHealthWeightGraphComponent implements OnInit {
   options = {}
-  mode = 0;
-  intervalHandler: number | null = null;
 
-  get data_raw() {
-    return HEALTH_WEIGHT;
-  }
   get data_keys() {
     return HEALTH_WEIGHT.map((data: any[]) => {
       return data[0];
     }).slice(1, HEALTH_WEIGHT.length)
   }
-
-  set Mode(val: number) {
-    this.mode = val;
-    if(this.mode == 0) {
-      this.options = options_barchart_polar(this.data_raw)
-    } else if (this.mode == 1) {
-      this.options = options_barchart(this.data_keys, {erfolg: this.data_erfolg, misserfolg: this.data_misserfolg})
-    } else {
-      this.options = options_barchart_stacked(this.data_keys, {erfolg: this.data_erfolg, misserfolg: this.data_misserfolg})
-    }
-  }
-
   get data_erfolg() {
     return HEALTH_WEIGHT.map((data: any[]) => {
       return data[1];
@@ -214,10 +88,7 @@ export class PmHealthWeightGraphComponent implements OnInit {
 
 
   constructor() {
-    this.options = options_barchart_polar(this.data_raw)
-    this.intervalHandler = window.setInterval(()=> {
-      this.Mode = (this.mode + 1) % 3
-    }, 10000)
+    this.options = this.options = options_barchart_stacked(this.data_keys, {erfolg: this.data_erfolg, misserfolg: this.data_misserfolg})
   }
 
   ngOnInit() {
